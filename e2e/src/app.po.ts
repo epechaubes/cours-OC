66ba9ad3f1f1b9ceb1ee117cd84f70ecfe5a7b96
epechaubes <emerick.pechaubes@ipilyon.net>
2019-06-24 14:28:55 +0200

    console.log()

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -3,6 +3,7 @@ import { browser, by, element } from 'protractor';
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
+    console.log("test")
   }
 
   getTitleText() {
